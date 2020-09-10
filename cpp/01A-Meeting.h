f13fbf6380a6f7a326196eb6ea5c06e4b2fab329
awpala <adam.w.pala@gmail.com>
2020-09-09 20:51:45 -0500

    Minor reformating for Meeting class

--- cpp/01A-Meeting.h
+++ cpp/01A-Meeting.h
@@ -3,50 +3,50 @@
 
 class Meeting
 {
-private:
-    // number of 30 min blocks past 9:00 am
-    unsigned int startTime_;
-    unsigned int endTime_;
+    private:
+        // number of 30 min blocks past 9:00 am
+        unsigned int startTime_;
+        unsigned int endTime_;
 
-public:
-    Meeting() :
-        startTime_(0),
-        endTime_(0)
-    {
-    }
+    public:
+        Meeting() :
+            startTime_(0),
+            endTime_(0)
+        {
+        }
 
-    Meeting(unsigned int startTime, unsigned int endTime) :
-        startTime_(startTime),
-        endTime_(endTime)
-    {
-    }
+        Meeting(unsigned int startTime, unsigned int endTime) :
+            startTime_(startTime),
+            endTime_(endTime)
+        {
+        }
 
-    unsigned int getStartTime() const
-    {
-        return startTime_;
-    }
+        unsigned int getStartTime() const
+        {
+            return startTime_;
+        }
 
-    void setStartTime(unsigned int startTime)
-    {
-        startTime_ = startTime;
-    }
+        void setStartTime(unsigned int startTime)
+        {
+            startTime_ = startTime;
+        }
 
-    unsigned int getEndTime() const
-    {
-        return endTime_;
-    }
+        unsigned int getEndTime() const
+        {
+            return endTime_;
+        }
 
-    void setEndTime(unsigned int endTime)
-    {
-        endTime_ = endTime;
-    }
+        void setEndTime(unsigned int endTime)
+        {
+            endTime_ = endTime;
+        }
 
-    bool operator==(const Meeting& other) const
-    {
-        return
-            startTime_ == other.startTime_
-            && endTime_ == other.endTime_;
-    }
+        bool operator==(const Meeting& other) const
+        {
+            return
+                startTime_ == other.startTime_
+                && endTime_ == other.endTime_;
+        }
 };
 
 #endif
